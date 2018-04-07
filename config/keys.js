07adebf5775df6073c02d2ad2ae092fc59f3c7b4
tingluhk <tingluhk@gmail.com>
2018-04-07 23:04:58 +0800

    added keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // keys. js - figure out what set of credentials to return
-if(process.env.Node_ENV=== 'production'){
+if(process.env.NODE_ENV === 'production'){
   // in pro
   module.exports = require( './prod.js' )
 }else{
